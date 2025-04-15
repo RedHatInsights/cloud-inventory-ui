d429ebfa0c489ecc35192dbab9a88d40271d59bf
Luke Green <lgreen@redhat.com>
2025-04-15 12:52:24 -0500

    lint:staged, rename main route, and remove comment from fec config

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -3,7 +3,7 @@ import { Route as RouterRoute, Routes as RouterRoutes } from 'react-router-dom';
 import { InvalidObject } from '@redhat-cloud-services/frontend-components/InvalidObject';
 import { Bullseye, Spinner } from '@patternfly/react-core';
 
-const SamplePage = lazy(
+const HelloPage = lazy(
   () =>
     import(
       /* webpackChunkName: "HelloCloudInventoryPage" */ './Pages/HelloCloudInventoryPage/HelloCloudInventoryPage'
@@ -30,7 +30,7 @@ const routes = [
   },
   {
     path: '/',
-    element: SamplePage,
+    element: HelloPage,
   },
   /* Catch all unmatched routes */
   {
