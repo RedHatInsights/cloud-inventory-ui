7e4e7e34fafb0f847440ae8c6fb199bd57a82ff1
blower <blower@redhat.com>
2026-05-08 13:51:41 -0400

    Fix cloud accounts provider query param mapping:
    -map Gold Images providers to Cloud Accounts shortName values
    -fix filtered cloud accounts links for Azure and GCP providers

--- src/Components/GoldImages/GoldImagesTable.tsx
+++ src/Components/GoldImages/GoldImagesTable.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
-import { GoldImagesResponse } from '../../hooks/api/useGoldImages';
+import {
+  CloudProviderName,
+  GoldImagesResponse,
+} from '../../hooks/api/useGoldImages';
 import {
   SortByDirection,
   Table,
@@ -24,6 +27,7 @@ import { hasPaginationError } from '../../utils/errors';
 import { PaginationError } from '../shared/PaginationError';
 import { Link } from 'react-router-dom';
 import { Paths } from '../../utils/routing';
+import { CloudProviderShortname } from '../../types/cloudAccountsTypes';
 
 interface GoldImagesProps {
   goldImages: GoldImagesResponse;
@@ -61,6 +65,12 @@ export const GoldImagesTable = ({ goldImages }: GoldImagesProps) => {
     (pageOptions.page - 1) * pageOptions.perPage,
     pageOptions.page * pageOptions.perPage,
   );
+
+  const goldImagesProviderToCloudAccountShortName = {
+    [CloudProviderName.AWS]: CloudProviderShortname.AWS,
+    [CloudProviderName.GCP]: CloudProviderShortname.GCP,
+    [CloudProviderName.AZURE]: CloudProviderShortname.AZURE,
+  };
 
   useEffect(() => {
     setGoldImagePagination({
@@ -101,9 +111,16 @@ export const GoldImagesTable = ({ goldImages }: GoldImagesProps) => {
               </Td>
               <Td modifier="fitContent">
                 <Link
-                  to={`../${Paths.CloudAccounts}?${generateQueryParamsForData([hyperscaler.provider], 'shortName').toString()}`}
+                  to={`../${Paths.CloudAccounts}?${generateQueryParamsForData(
+                    [
+                      goldImagesProviderToCloudAccountShortName[
+                        hyperscaler.provider
+                      ],
+                    ],
+                    'shortName',
+                  ).toString()}`}
                 >
-                  View cloud accounts
+                    View cloud accounts
                 </Link>
               </Td>
             </Tr>
