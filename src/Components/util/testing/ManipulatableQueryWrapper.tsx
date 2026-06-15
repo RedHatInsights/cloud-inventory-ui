66b942ed1d61eb7c5f5aa9322842fcf3501a2394
Luke Green <lgreen@redhat.com>
2026-06-15 09:06:17 -0500

    TUSC-551: Remove unused packages (#198)
    * Remove unused packages
    
    This is a newer app, so there aren't nearly as many unused packages.
    However, this removes the ones that are
    
    * try with hook from render
    
    * try with retry:false
    
    * and with staleTime inf

--- src/Components/util/testing/ManipulatableQueryWrapper.tsx
+++ src/Components/util/testing/ManipulatableQueryWrapper.tsx
@@ -1,7 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: Infinity,
+    },
+  },
+});
 
 export const ManipulatableQueryWrapper = (children: JSX.Element) => ({
   ComponentWithQueryClient: () => (
