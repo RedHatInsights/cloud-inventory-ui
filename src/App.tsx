6bdc2be7b27d8afd2449b7257e9e57db1cfce5d3
blower <blower@redhat.com>
2026-07-08 12:12:18 -0400

    Wrapped the app in AccessCheckProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Routing from './Routing';
 import './App.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import NotificationsProvider from '@redhat-cloud-services/frontend-components-notifications/NotificationsProvider';
+import { AccessCheck } from '@project-kessel/react-kessel-access-check';
 
 const queryClient = new QueryClient();
 
@@ -18,12 +19,16 @@ const App = () => {
 
   return (
     <Fragment>
-      <QueryClientProvider client={queryClient}>
-        <NotificationsProvider />
-        <Routing />
-      </QueryClientProvider>
+      <AccessCheck.Provider
+        baseUrl={window.location.origin}
+        apiPath="/api/kessel/v1beta2"
+      >
+        <QueryClientProvider client={queryClient}>
+          <NotificationsProvider />
+          <Routing />
+        </QueryClientProvider>
+      </AccessCheck.Provider>
     </Fragment>
   );
 };
-
 export default App;
